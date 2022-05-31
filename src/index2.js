c98497fd93ee843325fb6d6b06969f8036ed83fc
Ina <95903764+Ina-dang@users.noreply.github.com>
2022-05-31 22:27:27 +0900

    220531 자스 함수

---
+++ src/index2.js
@@ -0,0 +1,15 @@
+function getArea() {
+  let width = 10;
+  let height = 20;
+
+  let area = width * height;
+  console.log(area);
+}
+
+getArea();
+console.log("함수 실행 완료!!");
+getArea();
+getArea();
+getArea();
+getArea();
+getArea();
