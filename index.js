813c987bf2aa10c80b57bee490664a11534b5872
Ina-dang <ina9377@gmail.com>
2022-06-21 18:45:11 +0900

    220621 npm 외부모듈 사용하기

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const calc = require('./calc');
 console.log(calc.add(1,2));
 console.log(calc.add(4,5));
 console.log(calc.sub(10,2));
+console.log("Hello Node.js package");
---
+++ randomColor.js
@@ -0,0 +1,10 @@
+
+                            //랜덤컬러 외부모듈
+const randomColor = require('randomcolor');
+
+let color1 = randomColor();
+let color2 = randomColor();
+let color3 = randomColor();
+
+
+console.log(color1, color2, color3);
