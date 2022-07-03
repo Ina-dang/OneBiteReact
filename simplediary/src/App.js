dd4bfc5e76db7eea371946d3191cee9b6a5ea91c
Ina-dang <ina9377@gmail.com>
2022-07-03 18:40:57 +0900

    220703 List형태로 랜더링하기

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,11 +1,46 @@
-import './App.css';
+import React, {useState, useEffect} from 'react';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+import './App.css';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이나당",
+    content: "첫일기",
+    emotion: 4,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "길동당",
+    content: "둘일기",
+    emotion: 3,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "곰당",
+    content: "삼일기",
+    emotion: 2,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 4,
+    author: "지당",
+    content: "넷일기",
+    emotion: 1,
+    created_date: new Date().getTime()
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <DiaryEditor></DiaryEditor>
+      <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
+
   );
 }
 
