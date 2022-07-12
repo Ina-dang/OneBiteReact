828f130ff06ea4654861aa61d43c1774a50cf6e9
Ina-dang <ina9377@gmail.com>
2022-07-12 21:25:05 +0900

    220712 useMemo

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 //import Lifecycle from './Lifecycle';
@@ -46,7 +46,7 @@ function App() {
 
   const getData = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
-    console.log(res);
+    // console.log(res);
 
     const initData = res.slice(0, 20).map((it) => {
       return {
@@ -89,12 +89,27 @@ function App() {
     setData(newDiaryList);
   }
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, badCount, goodRatio }
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 좋은 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
-
   );
 }
 
