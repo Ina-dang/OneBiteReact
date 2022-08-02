14ea905de038227d6eac6471499146dcf271fad8
Ina-dang <ina9377@gmail.com>
2022-08-02 20:18:17 +0900

    공통 컴포넌트 등록

--- emotiondiary/src/App.js
+++ emotiondiary/src/App.js
@@ -4,20 +4,47 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import RounteTest from './components/RouteTest';
+
+// COMPONENTS
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
+
 
 function App() {
+
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"여기가 헤드공간이 될겁니다"}
+          leftChild={<MyButton text={"왼쪽버튼"} onClick={() => alert("왼쪽클릭")} />}
+          rightChild={<MyButton text={"오른쪽버튼"} onClick={() => alert("오른쪽클릭")} type={"negative"} />}
+        />
         <h2>App.js</h2>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+        />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RounteTest />
       </div>
     </BrowserRouter>
   );
